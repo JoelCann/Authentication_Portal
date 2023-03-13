1c8b35632f0817e0856a9468d8747cc4532414a8
JoelCann <joelcann12@gmail.com>
2023-03-13 13:36:46 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import login from './login.svg'
+
 
 function App() {
+
+  const[email,setEmail]=useState('')
+  const[password, setPassword]=useState('')
+
+  const validateSubmit=(e)=> {
+    
+    if (email === "jcann8691@gmail.com"  &&  password === "beans1")
+      alert('Successful login')
+     
+    else 
+    e.preventDefault()
+        alert('Invalid Credentials')
+        e.preventDefault()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>Authentication Portal</h1>
+      
+      <div className='container'>
+          <div className='row'>
+            <div className='col-md-6  '>
+              <img className='float-md-right float-end p-5'  src={login}   width='400'/>
+            </div>
+            <div className='col-md-6 my-auto bg-dark p-5 w-25 rounded '>
+              <form onSubmit={validateSubmit}>
+                  <div className="mb-3 ">
+                    <label className="form-label " >Email address</label>
+                    <input type="email" className="form-control" placeholder='Input email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+                  </div>
+
+                  <div className="mb-3 ">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control" placeholder='Input Password' value={password} onChange={(e)=> setPassword(e.target.value)} />
+                  </div>
+                  <button type="submit" className="btn btn-primary" >Submit</button>
+              </form>
+            </div>
+
+          </div>
+      </div>
+      
+      
     </div>
   );
 }
